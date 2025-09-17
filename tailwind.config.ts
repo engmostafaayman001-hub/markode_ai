5876f6f72a505bd37383ef2bf514797be2b84762
kayanalqusur-ai <kayanalqusur@gmail.com>
2025-09-17 05:49:44 +0300

    markode uodate

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./client/index.html",
     "./client/src/**/*.{js,jsx,ts,tsx}"
